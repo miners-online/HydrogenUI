887428269854590ab21733fd8cbe918b2c8434ea
ajh123 <ajh123@users.noreply.github.com>
2024-08-16 20:20:35 +0100

    fix: use newer `terser` + solves `legacy-peer-deps`

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import packageJson from './package.json' assert { type: 'json' };
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import external from 'rollup-plugin-peer-deps-external';
 import dts from 'rollup-plugin-dts';
 
